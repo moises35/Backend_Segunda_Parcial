5c3d169cb3f12aa79cdc9c7e7f4bd29c1e47295f
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-05-04 19:01:08 -0400

    Model for "restaurante"

---
+++ models/restaurante.model.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./../configs/sequelize.config');
+
+const Restaurante = sequelize.define('restaurante', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    direccion: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = Restaurante;
