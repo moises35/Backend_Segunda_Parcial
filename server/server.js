186edb560fae7ca5580c10d30242e07f5f5da8c7
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-05-04 22:16:39 -0400

    Routes for "mesa"

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/restaurante', require('./routes/restaurante.routes'));
+app.use('/api/mesa', require('./routes/mesa.routes'));
 
 
 
