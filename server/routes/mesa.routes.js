5fe979cabc4bc17bc239abe8b5da6a066adf4e81
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-05-04 22:16:22 -0400

    Routes for "mesa"

---
+++ server/routes/mesa.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const mesa_controller = require('./../controllers/mesa.controllers');
+
+// Rutas
+router.get('/', mesa_controller.findAll);
+router.get('/:id', mesa_controller.findById);
+router.post('/', mesa_controller.create);
+router.put('/:id', mesa_controller.update);
+router.delete('/:id', mesa_controller.eliminar);
+
+module.exports = router;
