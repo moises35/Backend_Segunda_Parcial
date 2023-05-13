694850a14119f58e2b839f6a50ce4ead42ce7ae9
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-05-13 12:46:32 -0400

    Routes for "reserva"

---
+++ server/routes/reserva.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const reserva_controller = require('./../controllers/reserva.controllers');
+
+// Rutas
+router.get('/', reserva_controller.obtenerReservas);
+router.get('/:idRestaurante/:fecha/:horaInicio/:horaFin', reserva_controller.listarMesasDisponibles);
+router.post('/', reserva_controller.crearReserva);
+
+module.exports = router;
