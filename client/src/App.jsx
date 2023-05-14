96993c28e5cf1b8de3dc13d00f2e81be50b68e39
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-05-13 20:23:20 -0400

    Se agrego un header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Cliente from "./views/Cliente";
 
 function App() {
 	return (
-		<div className="App">
-			
-		</div>
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes> 
+					<Route path="/" element={<Home />} />
+					<Route path="/cliente" element={<Cliente />} />
+					<Route path="/restaurante" element={<p>Restaurante</p>} />
+					<Route path="/mesa" element={<p>Mesa</p>} />
+					<Route path="/reserva" element={<p>Reserva</p>} />
+					<Route path="*" element={<p>Error</p>} /> 
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+    background-color: #232323;
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 80px;
+    }
+`;
+
+const NavLinkStyled = styled(NavLink)`
+    color: #ffffff;
+    font-size: 1.2rem;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    transition: all 0.3s ease;
+    &:hover {
+        background-color: #ffffff;
+        color: #232323;
+        border-radius: 5px;
+    }
+`;
+
+const Header = () => {
+    return (
+        <Nav>
+            <ul>
+                <li>
+                    <NavLinkStyled exact to="/">Inicio</NavLinkStyled>
+                </li>
+                <li>
+                    <NavLinkStyled to="/cliente">Clientes</NavLinkStyled>
+                </li>
+                <li>
+                    <NavLinkStyled to="/restaurante">Restaurantes</NavLinkStyled>
+                </li>
+                <li>
+                    <NavLinkStyled to="/mesa">Mesas</NavLinkStyled>
+                </li>
+                <li>
+                    <NavLinkStyled to="/reserva">Reservas</NavLinkStyled>
+                </li>
+            </ul>
+        </Nav>
+    );
+};
+
+export default Header;
